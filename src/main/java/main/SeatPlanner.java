dc12234a642c75382005732d8b48a87cab54d750
Matthew Farrow <matt.dg.farrow@gmail.com>
2019-12-02 13:00:21 +0000

    Second commit

--- src/main/java/main/SeatPlanner.java
+++ src/main/java/main/SeatPlanner.java
@@ -3,36 +3,29 @@ package main;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 public class SeatPlanner {
 
 	List<String> initials = new ArrayList<String>();
-	
-	String[] initialArray = {"AM","AR","AT","AW","CA","CG","ER","ED","HH","JB","JK","JL","JG","LM","EC","LP","LC","LH","MF","NV","RR","TK","ZZ", "XX"};
-			
 
-			
-	public void shuffle() {
-		
-		Collections.addAll(initials, initialArray);
+	public void shuffle(String... allInitials) {
+
+		Collections.addAll(initials, allInitials);
 
 		Collections.shuffle(initials);
 
 		int rowCount = 2;
-		System.out.println("Front");		
-		System.out.println("Row 1:                               Window");
+		System.out.println("Front");
+		System.out.println("Row 1:                                Window");
 		for (int k = 0; k < initials.size(); k++) {
 
-			
 			System.out.print(initials.get(k) + "     ");
-			
-			if ((k+1)%6==0 && k<(initials.size()-1)) {
-//				System.out.print("Window");
+
+			if ((k + 1) % 6 == 0 && k < (initials.size() - 1)) {
 				System.out.println("\n" + "Row " + rowCount + ": ");
 				rowCount++;
 			}
-			
+
 		}
 		System.out.print("\nBack");
 	}
